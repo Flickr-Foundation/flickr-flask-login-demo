725b123591ecd87b383bd5de2bb7b91c83db18d0
Alex Chan <alex@alexwlchan.net>
2024-10-21 12:29:07 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,3 @@
+"""
+A demo Flask app.
+"""
---
+++ tests/test_truth.py
@@ -0,0 +1,8 @@
+"""Stub test suite."""
+
+
+def test_truth() -> None:
+    """
+    Truth can be defined ~ some philosopher, probably.
+    """
+    assert True
